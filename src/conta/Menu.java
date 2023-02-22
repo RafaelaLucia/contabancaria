fefe46c357d39bffc894d79312466f4f753b580f
Rafaela Lucia <rafaluciadev@gmail.com>
2023-02-22 13:48:13 -0300

    Adicao de subclasses contaCorrente e contaPoupanca

--- src/conta/Menu.java
+++ src/conta/Menu.java
@@ -1,10 +1,39 @@
 package conta;
 
 import java.util.Scanner;
+import conta.model.Conta;
+import conta.util.Cores;
+import conta.model.ContaCorrente;
+import conta.model.ContaPoupanca;
 
 public class Menu {
 
 	public static void main(String[] args) {
+		
+		//teste da classe conta
+		Conta c1 = new Conta(1, 123, 1, "Adriana", 10000.0f);
+		c1.visualizar();
+		c1.sacar(12000.0f);
+		c1.visualizar();
+		c1.depositar(5000.0f);
+		c1.visualizar();
+		
+		//Teste da classe corrente
+		ContaCorrente cc1 = new ContaCorrente(2,123,1,"Mariana",15000.0f,1000.0f);
+		cc1.visualizar();
+		cc1.sacar(12000.0f);
+		cc1.visualizar();
+		cc1.depositar(5000.0f);
+		cc1.visualizar();
+		
+		//Teste da classe poupanca
+		ContaPoupanca cp1 = new ContaPoupanca(3,123,2,"Vitor",100000.0f,15);
+		cp1.visualizar();
+		cp1.sacar(1000.0f);
+		cp1.visualizar();
+        cp1.depositar(5000.0f);
+        cp1.visualizar();
+		
 	
 		Scanner leia = new Scanner(System.in);
 		
@@ -12,7 +41,7 @@ public class Menu {
 		
 		while(true) {
 			
-			System.out.println(""+
+			System.out.println(Cores.TEXT_YELLOW + Cores.ANSI_BLACK_BACKGROUND + ""+
 		"________________________________________________\n"+
 		"|                                              |\n"+
 	    "|           BANCO DO BRAZIL ZIL ZIL            |\n"+
@@ -30,11 +59,11 @@ public class Menu {
 		"|                                              |\n"+
 		"|______________________________________________|\n"+
 		"\n"+
-		"Entre com a opção desejada:\n");
+		"Entre com a opção desejada:\n" + Cores.TEXT_RESET);
 			opcao = leia.nextInt();
 			
 			if(opcao == 9) {
-				System.out.println("\n Banco do Brazil zil zil - O seu futuro começa aqui!");
+				System.out.println(Cores.TEXT_WHITE_BOLD + "\n Banco do Brazil zil zil - O seu futuro começa aqui!");
 				leia.close();
 				System.exit(0);
 			}
@@ -42,31 +71,31 @@ public class Menu {
 			
 			switch(opcao) {
 			case 1:
-				System.out.println("Criar conta \n\n");
+				System.out.println(Cores.TEXT_WHITE_BOLD + "Criar conta \n\n");
 				break;
 			case 2:
-				System.out.println("Listar todas as Contas\n\n");
+				System.out.println(Cores.TEXT_WHITE_BOLD + "Listar todas as Contas\n\n");
 				break;
 			case 3:
-				System.out.println("Consultar dados da Conta - por número\n\n");
+				System.out.println(Cores.TEXT_WHITE_BOLD + "Consultar dados da Conta - por número\n\n");
 				break;
 			case 4:
-				System.out.println("Atualizar dados da Conta\n\n");
+				System.out.println(Cores.TEXT_WHITE_BOLD + "Atualizar dados da Conta\n\n");
 				break;
 			case 5:
-				System.out.println("Apagar a Conta\n\n");
+				System.out.println(Cores.TEXT_WHITE_BOLD + "Apagar a Conta\n\n");
 				break;
 			case 6:
-				System.out.println("Saque\n\n");
+				System.out.println(Cores.TEXT_WHITE_BOLD + "Saque\n\n");
 				break;
 			case 7:
-				System.out.println("Depósito\n\n");
+				System.out.println(Cores.TEXT_WHITE_BOLD + "Depósito\n\n");
 				break;
 			case 8:
-				System.out.println("Transferência entre Contas\n\n");
+				System.out.println(Cores.TEXT_WHITE_BOLD + "Transferência entre Contas\n\n");
 				break;
 			default:
-				System.out.println("\nOpção Inválida\n\n");
+				System.out.println(Cores.TEXT_RED_BOLD + "\nOpção Inválida\n\n" + Cores.TEXT_RESET);
 				break;
 			}
 			
